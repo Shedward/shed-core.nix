1a58c7659af22eb5f1bc7b62009e788ba97c3138
Vlad Maltsev <shedwardx@gmail.com>
2025-01-21 22:00:13 +0300

    Fix wrong hash

--- features/torrents/transmission-patches.overlay.nix
+++ features/torrents/transmission-patches.overlay.nix
@@ -3,7 +3,7 @@ self: super: {
     patches = oldAttrs.patches or [] ++ [
       ((import super.path {}).fetchpatch2 {
         url = "https://gist.githubusercontent.com/Shedward/4dc8c211ce57501520802b3a3753ad86/raw/b13cc45ea6bde6dfe250fbf601e814ea00b099dd/transmission-announce-hack.patch";
-        sha256 = "00pqb23mia9zdkp4cgqwxnbqg4swjb7r8jsqbsl56qwjb2l0c7c7";
+        sha256 = "sha256-PmFnCKmwiq/2hR9E2zLqdfikGa5d0fBPwqY2SamnrsA=";
       })
     ];
   });
